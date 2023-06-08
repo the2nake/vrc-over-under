be0a50dd3c3f8b37baf28d6167a7b470d97c6aa9
the2nake <home.vn2007@gmail.com>
2023-06-08 10:26:58 +0800

    feat: added some angle manipulation functions

--- src/common.hpp
+++ src/common.hpp
@@ -4,6 +4,8 @@
 
 #include "main.h"
 
+#include <cmath>
+
 double rpm_from_gearset(pros::motor_gearset_e_t gearing);
 
 void scale_down_magnitude(double &a, double &b, double max);
@@ -33,3 +35,21 @@ T mod(T x, T mod)
 }
 
 double shorter_turn(double h_0, double h_f, double circum);
+
+template <typename T>
+T in_radians(T deg)
+{
+    return 0.01745329251 * deg;
+}
+
+template <typename T>
+T sin_deg(T deg)
+{
+    return std::sin(in_radians(deg));
+}
+
+template <typename T>
+T cos_deg(T deg)
+{
+    return std::cos(in_radians(deg));
+}
